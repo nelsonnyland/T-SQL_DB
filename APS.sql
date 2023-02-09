39adf1ee1a6edf92ca707c53b0a0746661fc03c6
nelsonnyland <nelsonnyland@gmail.com>
2023-02-09 14:32:17 -0800

    APS

---
+++ APS.sql
@@ -0,0 +1,70 @@
+/*************************************************************
+**************************************************************
+** Author: Nelson Nyland									**
+** Date Modified: 2/9/23									**
+** Course: TCSS 445											**
+** Database Project											**
+**															**
+** Auto Parts Store Schema (APS)							**
+**															**
+** This database supports APS inventory	management and		**
+** vendor information.										**
+**															**
+**************************************************************
+*************************************************************/
+
+USE MASTER
+GO
+
+IF DB_ID('APS') IS NOT NULL DROP DATABASE APS
+GO
+
+CREATE DATABASE APS
+GO
+
+USE APS
+GO
+
+CREATE TABLE [AUTO]
+(
+	ID				int				NOT NULL	PRIMARY KEY IDENTITY(1000000000, 1)
+	,AutoMake		varchar(50)		NOT NULL	CHECK(LEN(AutoMake) > 1)
+	,AutoModel		varchar(50)		NOT NULL	
+	,AutoYear		int				NOT NULL	CHECK(LEN(AutoYear) = 4)
+	,AutoEngine		varchar(50)		NOT NULL	CHECK(LEN(AutoEngine) > 1)
+)
+GO
+
+CREATE TABLE VENDOR
+(
+	ID				int				NOT NULL	PRIMARY KEY IDENTITY(1000000000, 1)
+	,VendorName		varchar(50)		NOT NULL	CHECK(LEN(VendorName) > 1)
+)
+GO
+
+CREATE TABLE PART
+(
+	ID				int				NOT NULL	PRIMARY KEY IDENTITY(1000000000, 1)
+	,AutoID			int				NOT NULL	FOREIGN KEY REFERENCES [AUTO](ID)
+	,VendorID		int				NOT NULL	FOREIGN KEY REFERENCES VENDOR(ID)
+	,PartName		varchar(50)		NOT NULL	CHECK(LEN(PartName) > 1)
+	,PartDesc		varchar(250)	NULL		
+	,PartQty		int				NOT NULL	DEFAULT 0
+	,PartPrice		smallmoney		NOT NULL	CHECK(PartPrice > 0)
+)
+GO
+
+CREATE TABLE MERCHANDISE
+(
+	ID				int				NOT NULL	PRIMARY KEY IDENTITY(1000000000, 1)
+	,VendorID		int				NOT NULL	FOREIGN KEY REFERENCES VENDOR(ID)
+	,MerchName		varchar(50)		NOT NULL	CHECK(LEN(MerchName) > 1)
+	,MerchDesc		varchar(250)	NULL		
+	,MerchQty		int				NOT NULL	DEFAULT 0
+	,MerchPrice		smallmoney		NOT NULL	CHECK(MerchPrice > 0)
+)
+GO
+
+/************************************************************
+** \ \ \ \ \ \ \ \ \ \ \ \ END / / / / / / / / / / / / / / **
+*************************************************************/
