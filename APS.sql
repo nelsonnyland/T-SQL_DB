02927c951b8275b2934bd83cbd416672db55c2e1
Arashpreet123 <51033451+Arashpreet123@users.noreply.github.com>
2023-02-10 18:27:29 -0800

    updated version

--- APS.sql
+++ APS.sql
@@ -1,11 +1,12 @@
 /*************************************************************
 **************************************************************
-** Author: Nelson Nyland									**
+** Author: Nelson Nyland, Arashpreet S. Pandher,			**
+** Aman Vahora, Steven Le									**
 ** Date Modified: 2/9/23									**
 ** Course: TCSS 445											**
 ** Database Project											**
 **															**
-** Auto Parts Store CREATE (APS)							**
+** Auto Parts Store Schema (APS)							**
 **															**
 ** This database supports APS inventory	management and		**
 ** vendor information.										**
@@ -45,10 +46,10 @@ GO
 CREATE TABLE PART
 (
 	ID				int				NOT NULL	PRIMARY KEY IDENTITY(1000000000, 1)
-	,AutoID			int				NOT NULL	FOREIGN KEY REFERENCES [AUTO](ID)	ON DELETE NO ACTION		ON UPDATE NO ACTION
-	,VendorID		int				NOT NULL	FOREIGN KEY REFERENCES VENDOR(ID)	ON DELETE NO ACTION		ON UPDATE NO ACTION
+	,AutoID			int				NOT NULL	FOREIGN KEY REFERENCES [AUTO](ID)
+	,VendorID		int				NOT NULL	FOREIGN KEY REFERENCES VENDOR(ID)
 	,PartName		varchar(50)		NOT NULL	CHECK(LEN(PartName) > 1)
-	,PartDesc		varchar(250)	NULL		DEFAULT ''
+	,PartDesc		varchar(250)	NULL		
 	,PartQty		int				NOT NULL	DEFAULT 0
 	,PartPrice		smallmoney		NOT NULL	CHECK(PartPrice > 0)
 )
@@ -57,13 +58,81 @@ GO
 CREATE TABLE MERCHANDISE
 (
 	ID				int				NOT NULL	PRIMARY KEY IDENTITY(1000000000, 1)
-	,VendorID		int				NOT NULL	FOREIGN KEY REFERENCES VENDOR(ID)	ON DELETE NO ACTION		ON UPDATE NO ACTION
+	,VendorID		int				NOT NULL	FOREIGN KEY REFERENCES VENDOR(ID)
 	,MerchName		varchar(50)		NOT NULL	CHECK(LEN(MerchName) > 1)
-	,MerchDesc		varchar(250)	NULL		DEFAULT ''
+	,MerchDesc		varchar(250)	NULL		
 	,MerchQty		int				NOT NULL	DEFAULT 0
 	,MerchPrice		smallmoney		NOT NULL	CHECK(MerchPrice > 0)
 )
 GO
+
+/************************************************************
+** * * * * * * * * * * * DATA INSERT * * * * * * * * * * * **
+*************************************************************/
+
+INSERT INTO [AUTO] 
+(AutoMake, AutoModel, AutoYear, AutoEngine) 
+VALUES
+	('Toyota', 'Corolla', 2015, '2.0 liter four-cylinder'),
+	('Toyota', 'Camry', 2019, '2.5L 4-Cylinder'),
+    ('Honda', 'Accord', 2020, '1.5L Turbo 4-Cylinder'),
+    ('Ford', 'Mustang', 2021, '5.0L V8'),
+    ('Chevrolet', 'Camaro', 2022, '6.2L V8'),
+    ('Nissan', 'Altima', 2019, '2.5L 4-Cylinder'),
+	('Jeep', 'Wrangler', 2021, '2.0L 4-Cylinder Turbo'),
+    ('Dodge', 'Charger', 2020, '6.4L V8'),
+    ('Tesla', 'Model S', 2021, 'Dual Motor All-Wheel Drive'),
+    ('Audi', 'A4', 2022, '2.0L 4-Cylinder Turbo')
+
+GO
+
+INSERT INTO VENDOR 
+(VendorName)
+VALUES
+	('Toyota'),
+	('Honda'),
+	('Ford'),
+	('Nissan'),
+	('Chevrolet'),
+	('BMW'),
+	('Dodge'),
+	('Audi'),
+	('Tesla'),
+	('Jeep')
+GO
+
+INSERT INTO PART
+(AutoID, VendorID, PartName, PartDesc, PartQty, PartPrice)
+VALUES
+	(1000000000, 1000000000, 'Spark Plug', 'Iridium spark plug for high performance engines', 50, 5.99),
+	(1000000001, 1000000001, 'Oil Filter', 'High-efficiency oil filter for protecting engine components', 100, 7.99),
+	(1000000002, 1000000002, 'Air Filter', 'High-flow air filter for improved engine breathing', 75, 9.99),
+	(1000000003, 1000000003, 'Brake Pad', 'Premium ceramic brake pad for better stopping power', 200, 29.99),
+	(1000000004, 1000000004, 'Battery', 'Maintenance-free battery for reliable starting power', 50, 99.99),
+	(1000000005, 1000000005, 'Radiator Hose', 'High-temperature resistant radiator hose for improved cooling', 150, 12.99),
+	(1000000006, 1000000006, 'Alternator', 'High-output alternator for improved electrical performance', 75, 199.99),
+	(1000000007, 1000000007, 'Ignition Coil', 'High-performance ignition coil for improved engine performance', 100, 44.99),
+	(1000000008, 1000000008, 'Throttle Body', 'High-flow throttle body for improved engine responsiveness', 50, 149.99),
+	(1000000009, 1000000009, 'Exhaust Manifold', 'Stainless steel exhaust manifold for improved exhaust flow', 25, 199.99)
+
+GO
+
+INSERT INTO MERCHANDISE
+(VendorID, MerchName, MerchDesc, MerchQty, MerchPrice)
+VALUES
+	(1000000000, 'Car Stickers', 'Vinyl car stickers with various designs', 150, 5.99),
+	(1000000001, 'Floor Mats', 'Custom-fit floor mats for various car models', 50, 19.99),
+	(1000000002, 'Air Freshener', 'Scented air fresheners in various fragrances', 100, 2.49),
+	(1000000003, 'Car Cover', 'Water-resistant car cover for all-weather protection', 25, 39.99),
+	(1000000004, 'Steering Wheel Cover', 'Leather steering wheel cover for added comfort', 75, 14.99),
+	(1000000005, 'Seat Covers', 'Stylish seat covers in various colors and patterns', 30, 24.99),
+	(1000000006, 'Car Sun Shade', 'Foldable car sun shade to protect from UV rays', 100, 7.99),
+	(1000000007, 'Windshield Wipers', 'High-quality windshield wipers for clear vision', 40, 12.49),
+	(1000000008, 'Dash Cam', 'High-definition dash cam with night vision', 20, 59.99),
+	(1000000009, 'Engine Oil', '10W-30 motor oil', 500, 29.99)
+GO
+
+
 
 /************************************************************
 ** \ \ \ \ \ \ \ \ \ \ \ \ END / / / / / / / / / / / / / / **
--- Queries.sql
+++ Queries.sql
@@ -1,9 +1,65 @@
+-- SQL Query 1) The purpose of this query is to join the auto, part, and vendor tables using their respective ID's. The expected result is a join of these 3 tables
+SELECT *
+FROM [AUTO]
+JOIN PART ON [AUTO].ID = PART.AutoID
+JOIN VENDOR ON PART.VendorID = VENDOR.ID
+-- SQL Query 2) The purpose of this query is to join auto and part tables using their id's. It then uses a nested query to get the auto years greater than 2010 and group it by automake
+-- and number of parts. The expected result is the automake column with its part numbers.
+SELECT AutoMake, COUNT(*) AS NumberOfParts
+FROM AUTO
+INNER JOIN PART ON [AUTO].ID = PART.AutoID
+WHERE AutoMake IN (SELECT AutoMake FROM [AUTO] WHERE AutoYear > 2010)
+GROUP BY AutoMake;
+-- SQL Query 3) The purpose of this query is to get the "Toyota" automake. The expected result is the columns in auto table with AutoMake Toyota.
+SELECT *
+FROM [AUTO]
+WHERE [AUTO].AutoMake = (SELECT VENDOR.VendorName
+                    from VENDOR
+                    WHERE VENDOR.VendorName = 'Toyota')
+-- SQL Query 4) The purpose of this query is to get the columns from the auto and parts tables along with no matches. The expected result is the output of the full outer join.
+SELECT *
+FROM [AUTO]
+FULL OUTER JOIN PART ON [AUTO].ID = PART.AutoID
+-- SQL Query 5) The purpose of this query is to get the names of the merchandise and parts table where the quantity is less than the average for both. The expected result is a combination of these two. 
+SELECT PartName FROM PART
+WHERE PartQty < (SELECT AVG(PartQty) FROM PART)
+UNION 
+SELECT MerchName FROM MERCHANDISE
+WHERE MerchQty < (SELECT AVG(MerchQty) FROM MERCHANDISE) 
+--SQL QUERY 6) b) purpose is to get the revenue for the vendors. c) Expected to get columns with vendorname and total revenue for each vendor name.
+SELECT V.VendorName, SUM(P.PartPrice * P.PartQty) + SUM(M.MerchPrice * M.MerchQty) AS Revenue
+FROM VENDOR V
+LEFT JOIN PART P ON V.ID = P.VendorID
+LEFT JOIN MERCHANDISE M ON V.ID = M.VendorID
+GROUP BY V.VendorName
+--SQL QUERY 7) b) If you are a car company and you need more than 100 parts for a certain car c) Will do a join on auto and part tables and get automodel with more than 100 car parts.
+SELECT [AUTO].AutoModel
+FROM [AUTO]
+	JOIN PART
+	ON PART.AutoID = AUTO.ID
+WHERE PART.PartQty > 100
+--SQL QUERY 8) b) purpose is for users to find parts with prices less than avg price. c) Expected result is join of some of the columns in part and auto table with price less than avg part price
+SELECT P.PartName, P.PartPrice, A.AutoMake, A.AutoModel, A.AutoYear
+FROM PART P 
+INNER JOIN AUTO A ON P.AutoID = A.ID
+WHERE P.PartPrice < (SELECT AVG(PartPrice) FROM PART);
+
+
+--SQL QUERY 9) b) purpose is to get average part price and merchandise price for each vendor. Useful when trying to compare price for each vendor. 
+-- c) Expected results was to be a join on auto, vendor, and merchandies to create a result table with vendor names and avg pricing for each vendor.
 SELECT VENDOR.VendorName, AVG(PART.PartPrice + MERCHANDISE.MerchPrice / 2) AS "Average Price"
 FROM VENDOR
-	JOIN PART
-	ON VENDOR.ID = PART.VendorID
-	JOIN MERCHANDISE
-	ON VENDOR.ID = MERCHANDISE.VendorID
-WHERE PART.PartPrice > 100
+    JOIN PART
+    ON VENDOR.ID = PART.VendorID
+    JOIN MERCHANDISE
+    ON VENDOR.ID = MERCHANDISE.VendorID
 GROUP BY VENDOR.VendorName
---ORDER BY PART.PartPrice DESC
+--SQL QUERY 10) b) purpose is for users to find a vendors avg quantity for parts and merchandise. Can be useful to see if a vendor is stocked up on parts and merch.
+--c) Expected results is join on VENDOR, PART, MERCHANDISE and a table that has vendorname and the avg partqty plus merchqty.
+SELECT VENDOR.VendorName, AVG(PART.PartQty + MERCHANDISE.MerchQty) AS "Average Quantity"
+FROM VENDOR
+    JOIN PART as part
+    ON VENDOR.ID = part.VendorID
+    JOIN MERCHANDISE
+    ON VENDOR.ID = MERCHANDISE.VendorID
+GROUP BY VENDOR.VendorName
